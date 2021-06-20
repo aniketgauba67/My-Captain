5681623eba5c5955f4fb29cd5819d723707f0dc9
aniketgauba67 <85576196+aniketgauba67@users.noreply.github.com>
2021-06-21 00:39:08 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 00:20:12 2021
+
+@author: admin
+"""
+
+#Area of Circle
+radius=float(input("Enter radius of Circle"))
+Area= 22/7*radius
+print("The area of the circle with radius" ,radius,"is:",Area)
---
+++ Task2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 00:34:23 2021
+
+@author: admin
+"""
+
+# Finding file extension
+name=input("Enter file name:")
+file_extension= name.split(".")
+print("the extension of the file is:",file_extension[1])
